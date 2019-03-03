4e772d9fca2354262df67d3540d4bda13e5e691f
root <veriskymj@yahoo.co.id>
2019-03-04 01:16:13 +0700

    add console view for AlphaServer, fixing AlphaServer, change docker-compose builds, add deployment command in README

--- Server/AlphaServer.sh
+++ Server/AlphaServer.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-apt-get install -y rsync
+while true
+do
+(echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed\|accepted" /root/nodeABC.log | wc -l; else echo "0"; fi; echo "failed attempt(s)") 
+echo $(echo "nodeXYZ has "; if [ -e /root/nodeXYZ.log ]; then grep -o -i "failed\|accepted" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo "failed attempt(s)") ) > /home/monitoring.log
+sleep 1
+done
