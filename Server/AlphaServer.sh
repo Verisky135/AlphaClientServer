bd8b28fe9409afa1d653bdd3d1c721cf28d31dc2
root <veriskymj@yahoo.co.id>
2019-03-04 08:43:19 +0700

    Reduce update interval from 1s to 0.3s, remove substring 'failed' from info because it also count accepted attemps.

--- Server/AlphaServer.sh
+++ Server/AlphaServer.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-(echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed\|accepted" /root/nodeABC.log | wc -l; else echo "0"; fi; echo "failed attempt(s)") 
-echo $(echo "nodeXYZ has "; if [ -e /root/nodeXYZ.log ]; then grep -o -i "failed\|accepted" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo "failed attempt(s)") ) > /home/monitoring.log
-sleep 1
+(echo $(echo "nodeABC has "; if [ -e /root/nodeABC.log ]; then grep -o -i "failed\|accepted" /root/nodeABC.log | wc -l; else echo "0"; fi; echo " attempt(s)") 
+echo $(echo "nodeXYZ has "; if [ -e /root/nodeXYZ.log ]; then grep -o -i "failed\|accepted" /root/nodeXYZ.log | wc -l; else echo "0"; fi; echo " attempt(s)") ) > /home/monitoring.log
+sleep 0.3
 done
